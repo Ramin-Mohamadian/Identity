422043dcef29eb271aaa26c6b6bd35cb8b5212d9
Ramin-Mohamadian <raminmohamadian2020@gmail.com>
2024-12-30 00:23:30 +0330

    Create Dto For Register User

---
+++ Identity/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+        public IActionResult Registe()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Identity/Data/Dto/RegisterDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Data.Dto
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password  { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
